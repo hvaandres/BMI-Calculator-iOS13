315c8be0fb82bfe36d58bc3815d14cfe7a5b6642
hvaandres <hvaandres@icloud.com>
2020-06-23 23:33:24 -0600

    new information added

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Alan Andres Haro on 6/23/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
